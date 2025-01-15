a51d7a20886fa344883c191afc35176827d5f78a
X Roid <97869723+XRoiDX@users.noreply.github.com>
2025-01-15 20:21:14 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -30,7 +30,7 @@ class Var(object):
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "6891428437").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'niggasura'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@niggasura'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
